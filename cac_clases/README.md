512f760aa124c4b925f2815e025e3e1ff4c9d635
JustineVN <84462284+JustineVN@users.noreply.github.com>
2022-09-15 18:40:46 -0300

    First commit clase 6 - react

---
+++ cac_clases/README.md
@@ -0,0 +1,1 @@
+Clase 6 : Crear app en React, modificar archivo app.js agregando alguna etiqueta de HTML. 
---
+++ cac_clases/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Let's start React - Comision 22801</h1>
+      </header> 
+    </div>
+  );
+}
+
+export default App;
